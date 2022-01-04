06dff804054f35e47323ff3928ae36f26700da58
Qiusheng Wu <giswqs@gmail.com>
2022-01-04 14:21:22 -0500

    Added support for direct link

--- apps/home.py
+++ apps/home.py
@@ -5,6 +5,15 @@ import leafmap.foliumap as leafmap
 def app():
     st.title("Home")
 
+    st.markdown(
+        """
+    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
+    To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
+    https://share.streamlit.io/giswqs/streamlit-template?page=upload
+
+    """
+    )
+
     m = leafmap.Map(locate_control=True)
     m.add_basemap("ROADMAP")
     m.to_streamlit(height=700)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap  # import your app modules here
+from apps import home, heatmap, upload  # import your app modules here
 
 st.set_page_config(layout="wide")
+
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
+apps = {"Home": "house", "Heatmap": "map", "Upload": "cloud-upload"}
+
+titles = [title.lower() for title in list(apps.keys())]
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    default_index = int(titles.index(params["page"][0].lower()))
+else:
+    default_index = 0
 
 with st.sidebar:
     selected = option_menu(
         "Main Menu",
-        ["Home", "Heatmap"],
-        icons=["house", "map"],
+        options=list(apps.keys()),
+        icons=list(apps.values()),
         menu_icon="cast",
-        default_index=1,
+        default_index=default_index,
     )
 
     st.sidebar.title("About")
@@ -25,7 +37,10 @@ with st.sidebar:
     """
     )
 
+# Place each app module under the apps folder
 if selected == "Home":
     home.app()
 elif selected == "Heatmap":
     heatmap.app()
+elif selected == "Upload":
+    upload.app()
