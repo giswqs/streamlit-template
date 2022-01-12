1ea2ea7ff3107d1298b9f14925784adfaf6ceabc
Qiusheng Wu <giswqs@gmail.com>
2022-01-12 16:33:02 -0500

    Updated gdf centroid

--- apps/upload.py
+++ apps/upload.py
@@ -60,7 +60,6 @@ def app():
             elif url:
                 file_path = url
                 layer_name = url.split("/")[-1].split(".")[0]
-            # st.write(f"Saved to {file_path}")
 
             with row1_col1:
                 if file_path.lower().endswith(".kml"):
@@ -68,8 +67,7 @@ def app():
                     gdf = gpd.read_file(file_path, driver="KML")
                 else:
                     gdf = gpd.read_file(file_path)
-                lon = gdf.centroid.iloc[0].x
-                lat = gdf.centroid.iloc[0].y
+                lon, lat = leafmap.gdf_centroid(gdf)
                 if backend == "pydeck":
 
                     column_names = gdf.columns.values.tolist()
@@ -81,14 +79,14 @@ def app():
                                 "Select a column to apply random colors", column_names
                             )
 
-                    m = leafmap.Map(center=(lat, lon))
+                    m = leafmap.Map(center=(40, -100))
+                    # m = leafmap.Map(center=(lat, lon))
                     m.add_gdf(gdf, random_color_column=random_column)
                     st.pydeck_chart(m)
 
                 else:
                     m = leafmap.Map(center=(lat, lon), draw_export=True)
                     m.add_gdf(gdf, layer_name=layer_name)
-                    # m.add_vector(file_path, layer_name=layer_name)
                     if backend == "folium":
                         m.zoom_to_gdf(gdf)
                     m.to_streamlit(width=width, height=height)
