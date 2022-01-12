7bafcf7398a2d8ea5a6da5cf1e3145d100816481
Qiusheng Wu <giswqs@gmail.com>
2022-01-12 15:28:46 -0500

    Updated postBuild

--- streamlit_call.py
+++ streamlit_call.py
@@ -6,7 +6,8 @@ def load_jupyter_server_extension(nbapp):
     Popen(
         [
             "streamlit",
-            "hello",
+            "run",
+            "streamlit_app.py",
             "--browser.serverAddress=0.0.0.0",
             "--server.enableCORS=False",
         ]
