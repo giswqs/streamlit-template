646e8b14aa171f3ccd79f03935becd2e7e333d51
Qiusheng Wu <giswqs@gmail.com>
2022-01-12 22:11:24 -0500

    Updated page title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from apps import home, heatmap, upload  # import your app modules here
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
